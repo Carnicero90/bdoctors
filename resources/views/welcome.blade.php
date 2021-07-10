13ff021b194a7671523c33a094f2915501d4456e
calavera <fmontani81@gmail.com>
2021-07-10 12:31:56 +0200

    rimosso dump di test da view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,9 +68,7 @@
 </head>
 
 <body>
-    @dump(
-        Auth::user()->reviews[0]->votes()
-        )
+
     <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
             <div class="top-right links">
