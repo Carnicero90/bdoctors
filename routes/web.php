be2365b6abe59278970801b4af944b1a31542a3f
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-07-10 12:33:08 +0200

    aggiunta vista della dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('admin.index');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
