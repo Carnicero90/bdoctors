8d031d9b64f7551debac59c031156bc1a40001a2
Mattia Bocchino <pmattiab@live.it>
2021-07-10 11:36:13 +0200

    eliminati dump di test in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,26 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <p>Voti</p>
-                @foreach (config('votes') as $item)
-                    @dump($item)
-                @endforeach
-                <p>Rece</p>
-                @dump(
-                    Auth::user()->reviews[0]
-                )
-                
+                <div class="card-header">{{ __('Dashboard') }}</div>                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{-- TOREMOVE --}}
-                    <h1>BOOLBARDS</h1>
-
                     {{ __('You are logged in!') }}
                 </div>
             </div>
