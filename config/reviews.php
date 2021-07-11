8b3268900a36c23c03384e41fc559e8f89c09871
Andrea Esposito <andexpox@gmail.com>
2021-07-11 19:33:59 +0200

    add config reviews

---
+++ config/reviews.php
@@ -0,0 +1,78 @@
+<?php
+return [
+    // TODO: 
+    //
+    // aggiungere descrizioni
+    //
+    // modificare user_id e vote_id ?
+    [
+        'user_id' => 1,
+        'vote_id' => 2,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 2,
+        'vote_id' => 5,
+        'author_name' => 'Gianfranco Paolozzi',
+        'author_email' => 'gianfpao@gmail.com',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 3,
+        'vote_id' => 3,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 4,
+        'vote_id' => 1,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 5,
+        'vote_id' => 4,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 6,
+        'vote_id' => 2,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 7,
+        'vote_id' => 3,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 8,
+        'vote_id' => 5,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 9,
+        'vote_id' => 1,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ],
+    [
+        'user_id' => 10,
+        'vote_id' => 5,
+        'author_name' => 'Ludovico Giamboni',
+        'author_email' => 'lud.giamb@hotmail.it',
+        'content' => 'lorem'
+    ]
+];
