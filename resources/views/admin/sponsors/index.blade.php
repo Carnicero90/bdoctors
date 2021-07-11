ce5d909c3eb2b9ac824b5e8f704ea3ba0186375e
Andrea Esposito <andexpox@gmail.com>
2021-07-11 16:58:14 +0200

    add carte per gli sponsor sia in index che in show

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -1,4 +1,37 @@
 @extends('layouts.app')
+
 @section('content')
     
+<div class="container">
+
+    {{-- TEST --}}
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Piano sponsor</h5>
+        <p class="card-text">Caccia i soldi.</p>
+        <a href="#" class="btn btn-primary">Vai al piano singolo</a>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Piano sponsor</h5>
+        <p class="card-text">Caccia i soldi.</p>
+        <a href="#" class="btn btn-primary">Vai al piano singolo</a>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">Piano sponsor</h5>
+        <p class="card-text">Caccia i soldi.</p>
+        <a href="#" class="btn btn-primary">Vai al piano singolo</a>
+        </div>
+    </div>
+
+</div>
+
 @endsection
