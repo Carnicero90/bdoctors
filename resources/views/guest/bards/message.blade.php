945b6dd12babdb986ba256a9d928d782bad7a8af
Mattia Bocchino <pmattiab@live.it>
2021-07-11 16:10:51 +0200

    add link per andare alle pagine messaggi e review

--- resources/views/guest/bards/message.blade.php
+++ resources/views/guest/bards/message.blade.php
@@ -1,38 +1,47 @@
 @extends('layouts.app')
-
 @section('content')
-    
     <div class="container">
-        <form action="{{ route('store-message') }}" method="post" enctype="multipart/form-data" class="form_class">
-            {{-- form_method == GET se  @method == GET, in ogni altro caso: form_method == PUT --}}
-            @csrf {{-- TOREMOVE: OBBLIGATORIO --}}
-            @method('POST')
-        
-            {{-- input#input_labelled --}}
-            <div class="form_group_class">
-                <label for="author_name">Autore</label>
-                <input type="text" class="input_class" id="author_name" name="author_name"
-                    value="" required>
-            </div>
-            {{-- END input.input_labelled --}}
-        
-            {{-- input#input_labelled --}}
-            <div class="form_group_class">
-                <label for="author_email">Email</label>
-                <input type="email" class="input_class" id="author_email" name="author_email"
-                    value="" required>
-            </div>
-            {{-- END input.input_no_label --}}
 
-            {{-- TextArea --}}
-            <div class="form_group_class">
-                <label for="text">Messaggio</label>
-                <textarea name="text" id="text" cols="30" rows="10" required></textarea>
+        <h1>Invia un messaggio a @{{Nome}} @{{Cognome}}</h1>
+
+        @include("partials.validation-errors")
+
+        {{-- form lascia una recensione --}}
+        <form action="{{route("store-message")}}" method="post" enctype="multipart/form-data">
+        {{-- form_method == GET se  @method == GET, in ogni altro caso: form_method == PUT --}}
+
+            @csrf {{-- TOREMOVE: OBBLIGATORIO --}}
+            @method("POST")
+
+            {{-- nome autore --}}
+            <div class="form-group mt-4 mb-4">
+                <label for="author_name">Autore</label>
+                <input type="text" class="form-control" id="author_name" name="author_name" placeholder="Inserisci il tuo nome" value="{{old("author_name")}}" required>
             </div>
-            {{-- END TextArea --}}
-        
-            <button type="submit" class="form_button_class">Invia Messaggio</button>
+
+            {{-- email autore --}}
+            <div class="form-group mt-4 mb-4">
+                <label for="author_email">Email</label>
+                <input type="email" class="form-control" id="author_email" name="author_email" placeholder="Inserisci il tuo indirizzo email" value="{{old("author_email")}}" required>
+            </div>
+
+            {{-- contenuto messaggio --}}
+            <div class="form-group mt-4 mb-4">
+                <label for="text">Testo messaggio</label>
+                <textarea class="form-control" name="text" id="text" rows="10" placeholder="Scrivi la recensione">{{old("text")}}</textarea>
+            </div>
+
+            {{-- tasto per inviare recensione --}}
+            <button type="submit" class="btn btn-primary mt-4">
+                Invia messaggio
+            </button>
+
         </form>
+
+        {{-- tasto per refreshare la pagina --}}
+        <div class="text-right">
+            <a class="btn btn-outline-dark" href="{{route("send-message")}}" style="transform: translateY(-38px)">Svuota i campi</a>
+        </div>
+        
     </div>
-    
 @endsection
