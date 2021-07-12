1b9d9a783893dd2e6a19b08de6f3bdc428335e4f
calavera <fmontani81@gmail.com>
2021-07-12 19:03:33 +0200

    aggiunto foreach per mostrare categorie in guest/show

--- resources/views/guest/bards/show.blade.php
+++ resources/views/guest/bards/show.blade.php
@@ -2,33 +2,38 @@
 
 @section('content')
     <div class="container">
+        <div>
+            @foreach ($user->categories as $cat)
+                @dump($cat->name)
+            @endforeach
+        </div>
         <h1>{{ $user->name }} {{ $user->lastname }}</h1>
         <p>E-mail: {{ $user->email }}</p>
         <p>Indirizzo: {{ $user->address }}</p>
         <div class="row">
             <div class="col-12">
                 @if ($user->profile)
-                <p> Vi parlo di me: {{ $user->profile->self_description }}</p>
-               
-                
-            @endif
+                {{-- TEST --}}
+                    <p> Vi parlo di me: {{ $user->profile->self_description }}</p>
+                @endif
             </div>
 
         </div>
 
         <h2>Recensioni</h2>
+        <a href="">Lascia una recensione</a>
 
         <div class="row">
             <div class="col-12">
                 <div class="card mt-4">
                     <div class="card-body">
                         @foreach ($user->reviews as $review)
-                            <div class="mt-2 mb-2"><span>da: {{$review->author_name}}</span></div>
-                            <div class="mt-2 mb-2"><span>email: {{$review->author_email}}</span></div>
-                            <div class="mt-2 mb-2"><span>voto: {{$review->vote_id}}</span></div>
+                            <div class="mt-2 mb-2"><span>da: {{ $review->author_name }}</span></div>
+                            <div class="mt-2 mb-2"><span>email: {{ $review->author_email }}</span></div>
+                            <div class="mt-2 mb-2"><span>voto: {{ $review->vote_id }}</span></div>
                             <div class="mt-2 mb-2">
                                 <span>Testo recensione:</span>
-                                <p class="card-text text-secondary">{{$review->content}}</p>
+                                <p class="card-text text-secondary">{{ $review->content }}</p>
                             </div>
                         @endforeach
                     </div>
