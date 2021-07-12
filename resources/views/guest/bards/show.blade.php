372454d0ca0c16d3145f5af28f73468be7ebf52d
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-12 17:50:24 +0200

    Aggiunte recensioni alla view user pubblica

--- resources/views/guest/bards/show.blade.php
+++ resources/views/guest/bards/show.blade.php
@@ -5,5 +5,25 @@
         <h1>{{ $user->name }} {{ $user->lastname }}</h1>
         <p>E-mail: {{ $user->email }}</p>
         <p>Indirizzo: {{ $user->address }}</p>
+
+        <h2>Recensioni</h2>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        @foreach ($user->reviews as $review)
+                            <div class="mt-2 mb-2"><span>da: {{$review->author_name}}</span></div>
+                            <div class="mt-2 mb-2"><span>email: {{$review->author_email}}</span></div>
+                            <div class="mt-2 mb-2"><span>voto: {{$review->vote_id}}</span></div>
+                            <div class="mt-2 mb-2">
+                                <span>Testo recensione:</span>
+                                <p class="card-text text-secondary">{{$review->content}}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
