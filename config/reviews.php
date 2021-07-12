9b5f3400be52eeba0b127f0697c33f5442f8cda8
Andrea Esposito <andexpox@gmail.com>
2021-07-12 10:32:56 +0200

    cambiato nomi autori in reviews

--- config/reviews.php
+++ config/reviews.php
@@ -22,57 +22,57 @@ return [
     [
         'user_id' => 3,
         'vote_id' => 3,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Giovanmaria Ravanetti',
+        'author_email' => 'giova.ravanetti@hotmail.it',
         'content' => 'lorem'
     ],
     [
         'user_id' => 4,
         'vote_id' => 1,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Amelia Amata',
+        'author_email' => 'amelia.amata@gmail.com',
         'content' => 'lorem'
     ],
     [
         'user_id' => 5,
         'vote_id' => 4,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Algemiro Randazzo',
+        'author_email' => 'algemiro.randazzo@outlook.it',
         'content' => 'lorem'
     ],
     [
         'user_id' => 6,
         'vote_id' => 2,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Gianna Calabrese',
+        'author_email' => 'giannacalabrese@rhyta.com',
         'content' => 'lorem'
     ],
     [
         'user_id' => 7,
         'vote_id' => 3,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Ferdinanda Romani',
+        'author_email' => 'ferdinanda.romani@jourrapide.com',
         'content' => 'lorem'
     ],
     [
         'user_id' => 8,
         'vote_id' => 5,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Elia Colombo',
+        'author_email' => 'elia.colombo@jourrapide.com',
         'content' => 'lorem'
     ],
     [
         'user_id' => 9,
         'vote_id' => 1,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Tullio Arcuri',
+        'author_email' => 'tullioarcuri@dayrep.com',
         'content' => 'lorem'
     ],
     [
         'user_id' => 10,
         'vote_id' => 5,
-        'author_name' => 'Ludovico Giamboni',
-        'author_email' => 'lud.giamb@hotmail.it',
+        'author_name' => 'Andrea Beneventi',
+        'author_email' => 'andrea.ben@dayrep.com',
         'content' => 'lorem'
     ]
 ];
