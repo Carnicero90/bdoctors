6c4522109a88fd0bdeb7d8235611ca90c7c74ad2
calavera <fmontani81@gmail.com>
2021-07-13 16:51:40 +0200

    aggiunte section per script e link ad axios, vue + script locali in admin/profile e /welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('header-scripts')
-    
+    {{-- Axios --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"
+        integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- Vuejs --}}
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+@endsection
+
+@section('footer-scripts')
+    <script src="{{ asset('js/guest/index.js') }}"></script>
 @endsection
 @section('content')
 
@@ -11,10 +20,11 @@
         </div>
 
         <div>
-            <a href="{{route("profile", ['id' => 1])}}" class="btn btn-success ml-2 mr-2">Pagina pubblica profilo di esempio</a>
-            <a href="{{route("sponsor-index")}}" class="btn btn-primary ml-2 mr-2">Vedi i piani di abbonamento</a>
+            <a href="{{ route('profile', ['id' => 1]) }}" class="btn btn-success ml-2 mr-2">Pagina pubblica profilo di
+                esempio</a>
+            <a href="{{ route('sponsor-index') }}" class="btn btn-primary ml-2 mr-2">Vedi i piani di abbonamento</a>
         </div>
-        
+
     </div>
 
 @endsection
