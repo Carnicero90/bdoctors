08c758edf4690798ad191e87a47aea3a8abc6cef
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-13 18:05:00 +0200

    Fatto performances.php in config e ServicesSeeder

---
+++ config/performances.php
@@ -0,0 +1,44 @@
+<?php 
+
+    // File con una lista di performances già creata da noi, da abbinare agli utenti
+    return [
+        [
+            'user_id' => 5,
+            'title' => 'Tournista',
+            'hourly_rate' => 100,00,
+            'description' => 'Stai per andare a fare un live e ti manca un componente? Ecco il tipo di performance che fa per te!',
+        ],
+        [
+            'user_id' => 8,
+            'title' => 'Tournista',
+            'hourly_rate' => 100,00,
+            'description' => 'Stai per andare a fare un live e ti manca un componente? Ecco il tipo di performance che fa per te!',
+        ],
+        [
+            'user_id' => 8,
+            'title' => 'Insegnante',
+            'hourly_rate' => 25,00,
+            'description' => 'Se stai imparando uno strumento e vuoi intraprendere un percorso di apprendimento, questo è il tipo di performance che fa al caso tuo!',
+        ],
+        [
+            'user_id' => 1,
+            'title' => 'Tecnico del suono',
+            'hourly_rate' => 150,00,
+            'description' => 'Se ti serve un professionista per rendere il tuo live indimenticabile, rivolgiti ad un Tecnico del suono!',
+        ],
+        [
+            'user_id' => 11,
+            'title' => 'Band',
+            'hourly_rate' => 10,00,
+            'description' => 'Se nella tua band hai un posto libero, perché non chiedere aiuto ad un professionista? Potreste fare amicizia e vivere insieme esperienze incredibili!',
+        ],
+        [
+            'user_id' => 9,
+            'title' => 'Autore',
+            'hourly_rate' => 50,00,
+            'description' => "Non trovi l'ispirazione giusta per i tuoi testi ma la tua musica spacca? Rivolgiti ad uno dei nostri bardi, che provvederà ad aiutrti con le tue liriche!",
+        ],
+        
+    ]
+
+ ?>
