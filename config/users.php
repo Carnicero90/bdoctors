6b39f68257023949b4759dce104fe0d4a121d51d
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-13 16:03:37 +0200

    Aggiunto 5 utenti

--- config/users.php
+++ config/users.php
@@ -91,6 +91,36 @@
             'address' => 'Viale Giuseppe Verdi, 77',
             'email' => 'manupiccolomini@email.it',
         ],
+        [
+            'name' => 'Barbara',
+            'lastname' => 'Scaranti',
+            'address' => 'Viale Padova, 18',
+            'email' => 'scarantibarb@email.it',
+        ],
+        [
+            'name' => 'Plinio',
+            'lastname' => 'Ascani',
+            'address' => 'Viale della Pala, 59',
+            'email' => 'plinissimo@email.it',
+        ],
+        [
+            'name' => 'Cianuro',
+            'lastname' => 'Bastioni',
+            'address' => 'Viale dei Cantori, 1',
+            'email' => 'ciaocianuro@email.it',
+        ],
+        [
+            'name' => 'Kaylash',
+            'lastname' => 'Sere',
+            'address' => 'Via Saffi, 22',
+            'email' => 'kaysere@email.it',
+        ],
+        [
+            'name' => 'Ivaldo',
+            'lastname' => 'Saccaramanni',
+            'address' => 'Via San Martino, 102',
+            'email' => 'ivaldoaldo@email.it',
+        ],
     ]
 
  ?>
