304921012353b93d6d340dfb73db636d3afa8db8
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-14 13:35:09 +0200

    Aggiunte Categories alla personalizzazione profilo privato

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -48,7 +48,7 @@
                     <label for="categories" class="d-block">Scegli le tue categorie</label>
                     @foreach ($categories as $category)
                         <div class="form-check d-inline-block mr-5 mb-2">
-                            <input class="form-check-input" name="categories" type="checkbox">
+                            <input value="{{$category->id}}" class="form-check-input" name="categories[]" type="checkbox" {{ Auth::user()->categories->contains($category) ? 'checked' : '' }}>
                             <label class="form-check-label" for="categories-{{$category->id}}">{{$category->name}}</label>
                         </div>
                     @endforeach
