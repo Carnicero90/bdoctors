e5b5858e84922b7204092f1ec584aca59e6bcbb8
calavera <fmontani81@gmail.com>
2021-07-14 07:37:33 +0200

    corretti refusi in Api/UserController@index

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -28,32 +28,32 @@ class UserController extends Controller
             'users.id', //lascio che magari poi serve come riferimento per un eventuale metodo di ricerca dettagliata del singolo user
             'users.name',
             'users.lastname',
-            'success',
-            'end_date',
+            // 'success',
+            // 'end_date',
+            DB::raw('avg(success) as success'),
+
+            DB::raw('avg(votes.value) as avg_vote')
         ])
-        //     'users.name',
-        //     'users.lastname',
-        //     'users.email',
-        //     DB::raw('max(sponsorplan_users.end_date) AS current_plan'),
-        //     DB::raw('avg(votes.value) as avg_vote')
-        // ])
-             ->where('users.name', 'LIKE', '%' . $user . '%')
-             ->orWhere('users.lastname', 'LIKE', '%' . $user . '%')
+            ->where('users.name', 'LIKE', '%' . $user . '%')
+            ->orWhere('users.lastname', 'LIKE', '%' . $user . '%')
 
             ->leftJoin('sponsorplan_users', function ($join) {
-                $join->on('sponsorplan_users.id', '=', 'users.id')
-                ->where('sponsorplan_users.success', '=', '1')
-                ->where('sponsorplan_users.end_date', '>', Carbon::now())
-                ;
+                $join->on('sponsorplan_users.user_id', '=', 'users.id')
+                    ->where('sponsorplan_users.success', '=', '1')
+                    ->where('sponsorplan_users.end_date', '>', Carbon::now());
             })
-            ->orderByDesc('end_date')
+
+            ->orderByDesc('success')
+            
+            ->leftJoin('reviews', 'users.id', '=', 'reviews.user_id')
+            ->leftJoin('votes', 'reviews.vote_id', '=', 'votes.id')
+            ->groupBy('users.id')
+            ->orderByDesc('avg_vote')
             ->get();
 
 
 
-        //     ->leftJoin('reviews', 'users.id', '=', 'reviews.user_id')
-        //     ->leftJoin('votes', 'reviews.vote_id', '=', 'votes.id')
-        //     ->groupBy('users.id')
+
 
 
         $data = [
