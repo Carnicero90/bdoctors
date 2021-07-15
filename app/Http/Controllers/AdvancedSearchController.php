9f3964b95ca6455fcad8c054a100cb0d3098332e
calavera <fmontani81@gmail.com>
2021-07-15 20:29:24 +0200

    aggiunto valore per filtrare in base alle recensioni ricevute in AdvancedSearchController@index

--- app/Http/Controllers/AdvancedSearchController.php
+++ app/Http/Controllers/AdvancedSearchController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Vote;
+use App\Review;
+use Illuminate\Support\Facades\DB;
 
 class AdvancedSearchController extends Controller
 {
     public function index(Request $request) {
-        // TODO: migliora sta merda
+        // review step tendenzialmente mi servira per dividere a seconda del n.ro di rece
         $votes = Vote::all();
+        $review_max = Review::select(DB::raw('COUNT(user_id) AS tot'))
+        ->groupBy('user_id')
+        ->orderByDesc('tot')
+        ->pluck('tot')
+        ->first();
+        $review_step = round($review_max/4);
 
-        return view('guest.advsearch.index', compact('votes'));
+        return view('guest.advsearch.index', compact('votes', 'review_step'));
     }
 
 }
