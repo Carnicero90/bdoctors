1cf15da1e03bafef218fa61f4aa644c0ebaeb886
calavera <fmontani81@gmail.com>
2021-07-16 01:00:48 +0200

    aggiunta favicon di prova e fix a popper.js not found in Safari (credo)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- TODO: favicon solo di test --}}
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
