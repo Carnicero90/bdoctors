9e2810b554715f9bdd7d9157b25f1d022f752326
Mattia Bocchino <pmattiab@live.it>
2021-07-15 18:37:55 +0200

    aggiustata dimensione foto profilo in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,19 +75,22 @@
         {{-- section sponsored --}}
         <div class="container">
             <h2 class="text-center">I nostri artisti del momento</h2>
-            <div class="row mt-4">
-                <div v-for="user in sponsoredUsers" class="col-3">
-                    <a :href="'bards/' + user.id" style="text-decoration: none">
-                        <div class="card mb-3">
+            <div class="row mt-4 d-flex justify-content-around">
+                <div v-for="user in sponsoredUsers" style="width: 21%">
+                    <a :href="'bards/' + user.id" style="text-decoration: none; color: #444;">
+                        <div class="card mb-4">
                             {{-- TODO: boh, magari la media voti la mostriamo solo se supera un tot? Pagano, non e' bellino per loro vedersi un
                             pallino solo come media recensioni (d'altra parte affari loro, bohbohboh) --}}
                             <div class="card-body d-flex flex-column align-items-center">
-                                <div style="width: 70px; height: 70px; border-radius: 50%; overflow: hidden;">
-                                    <img v-if="user.pic" :src="'/storage/' + user.pic" alt="" style="max-height: 70px; width: 100%; height: 100%; object-fit: cover;">
-                                    <img v-else src="http://127.0.0.1:8000/img/user-img.png" alt="" style="max-height: 70px;">
+                                <div style="width: 120px; height: 120px; border-radius: 50%; overflow: hidden;">
+                                    <img v-if="user.pic" :src="'/storage/' + user.pic" alt="" style="max-height: 120px; width: 100%; height: 100%; object-fit: cover;">
+                                    <img v-else src="http://127.0.0.1:8000/img/user-img.png" alt="" style="max-height: 120px;">
                                 </div>
-                                <div class="mt-3 mb-2">
+                                <div class="mt-3 mb-2 font-weight-bold">
                                     <span>@{{ user . name + ' ' + user . lastname }}</span>
+                                </div>
+                                <div class="text-secondary">
+                                    <small><p>categoria user</p></small>
                                 </div>
                                 <div>
                                     <span v-if="user.avg_vote > 0"><i v-for="n in parseInt(user.avg_vote)" class="fas fa-star"></i></span>
