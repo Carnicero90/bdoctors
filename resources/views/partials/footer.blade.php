53cf74741bc2031d0cffdcb62265fb673f251526
Andrea Esposito <andexpox@gmail.com>
2021-07-16 14:28:43 +0200

    bozza footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,60 @@
 {{-- TODO --}}
 <!-- FOOTER -->
 <footer>
-    <h2>FOOTER</h2>
+    <div class="jumbotrontest">
+        <div class="">
+            <!-- Logo -->
+            <div>
+                <img src="" alt="Author Logo">
+            </div>
+
+            <!-- About me -->
+            <div class="">
+                <div class="">
+                    <h4>About me</h4>
+
+                    <p>
+                        Vivamus suscipit <span>tortor eget felis porttitor</span> volutpat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat.
+                    </p>
+                </div>
+
+                <!-- Useful links -->
+                <div class="">
+                    <h4>Useful links</h4>
+
+                    <ul class="">
+                        <li v-for="item in usefulLink">
+                            <i class="fas fa-chevron-right"></i> 
+                        </li>
+                    </ul>
+                </div>
+                
+                <!-- Contact Info -->
+                <div class="">
+                    <h4>Contact info</h4>
+
+                    <ul>
+                        <li v-for="item in contact">
+                            
+                        </li>
+                    </ul>
+
+                    <div class="social-icons">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-linkedin-in"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright -->
+        <div class="copyright">
+            <i class="far fa-copyright"></i> Copyright 2020 - 2021 | Developed by <span>I Becci di BoolBards</span> | All Rights Reserved | Powered by <span>Laravel</span>
+        </div>
+    </div>
 </footer>
 <!-- END FOOTER -->
 
