1c42259cd08e97e589d7338157838bbd893b3fbb
calavera <fmontani81@gmail.com>
2021-07-16 13:54:15 +0200

    modifiche che ho scordato a layouts/app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Boolbards') }}</title>
     {{-- TODO: favicon solo di test --}}
     <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
 
