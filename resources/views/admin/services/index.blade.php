8ada4326ae73f15ea51232241fc9f2ead94dee06
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-17 11:56:18 +0200

    Facendo form per servizi

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -8,39 +8,46 @@
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 @endsection
 
+@section('footer-scripts')
+    <script src="{{ asset('js/profile.js') }}"></script>
+@endsection
+
 @section('content')
-    <div class="container">
-        <h1>Aggiungi o crea una prestazione</h1>
-
-        <form action="{{ route('admin.services-store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method('POST')
-
-            {{-- Nome Prestazione --}}
-            <div class="form-group mt-4">
-                <label for="service-name">Nome Prestazione</label>
-                <input type="text" 
-                       class="form-control"
-                       id="title" 
-                       :name="'service' + number + '[title]'"
-                       placeholder="Inserisci il nome del servizio">
-            </div>
-
-            {{-- Descrizione Prestazione --}}
-            <div class="form-group mt-4 mb-4">
-                <label for="description">Descrizione servizio</label>
-                <textarea class="form-control" :name="'service' + number + '[description]'" id="description"
-                    rows="3" placeholder="Descrivi il servizio offerto"></textarea>
-            </div>
-
-            {{-- Tariffa oraria --}}
-            <div class="form-group mt-4">
-                <label for="hourly_rate" class="d-inline-block mr-1">Tariffa oraria</label>
-                <input type="number" step="0.50" class="form-control d-inline-block" style="width: 100px;"
-                    id="hourly_rate" :name="'service' + number + '[hourly_rate]'" placeholder="00.00"
-                    min='0.00'>
-                <label class="d-inline-block ml-1">€</label>
-            </div>
-        </form>
+    <div class="root">
+        <div class="container">
+            <h1>Aggiungi o crea una prestazione</h1>
+    
+            <form action="{{ route('admin.services-store', ['id' => Auth::user()->id]) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @method('POST')
+    
+                {{-- Nome Prestazione --}}
+                <div class="form-group mt-4">
+                    <label for="service-name">Nome Prestazione</label>
+                    <input type="text" 
+                           class="form-control"
+                           id="title" 
+                           :name="'service' + number + '[title]'"
+                           placeholder="Inserisci il nome del servizio">
+                </div>
+    
+                {{-- Descrizione Prestazione --}}
+                <div class="form-group mt-4 mb-4">
+                    <label for="description">Descrizione servizio</label>
+                    <textarea class="form-control" :name="'service' + number + '[description]'" id="description"
+                        rows="3" placeholder="Descrivi il servizio offerto"></textarea>
+                </div>
+    
+                {{-- Tariffa oraria --}}
+                <div class="form-group mt-4">
+                    <label for="hourly_rate" class="d-inline-block mr-1">Tariffa oraria</label>
+                    <input type="number" step="0.50" class="form-control d-inline-block" style="width: 100px;"
+                        id="hourly_rate" :name="'service' + number + '[hourly_rate]'" placeholder="00.00"
+                        min='0.00'>
+                    <label class="d-inline-block ml-1">€</label>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
+
