e95d40a4b79c88a0a806986a319146a4fa79abe8
Mattia Bocchino <pmattiab@live.it>
2021-07-19 10:18:29 +0200

    piani sponsor

--- resources/views/guest/sponsors/index.blade.php
+++ resources/views/guest/sponsors/index.blade.php
@@ -4,20 +4,27 @@
 
     <div class="container">
 
-        <h1>Piani di sponsorizzazione</h1>
-
-        @foreach ($all_plans as $plan)
-            <div class="col-4 d-inline-block" style="width: 33%;">
-                <div class="card mt-4">
-                    <div class="card-body">
-                        <h4 class="card-title">Piano {{ucfirst(str_replace("_", " ", $plan->name))}}</h4>
-                        <div class="mb-2"><p class="card-text text-secondary">{{$plan->description}}</p></div>
-                        <div class="mb-4"><span class="card-text">Durata {{$plan->duration_in_hours}} ore - </span><span>Prezzo € {{$plan->pricing}}</span></div>
-                        <div><a href="{{route('sponsor-show', ['slug' => $plan->slug])}}" class="btn btn-primary">Vedi dettagli piano</a></div>
+        <div class="mb-4">
+            <h1>Piani di sponsorizzazione</h1>
+        </div>
+        
+        <div class="row">
+            @foreach ($all_plans as $plan)
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center justify-content-between">
+                            <h3 class="card-title">{{ucfirst(str_replace("_", " ", $plan->name))}}</h3>
+                            <h5>€ {{$plan->pricing}}</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="mb-2"><p class="card-text text-secondary">{{$plan->description}}</p></div>
+                            <div class="mb-4"><span class="card-text">Durata {{$plan->duration_in_hours}} ore</span></div>
+                            <div><a href="{{route('sponsor-show', ['slug' => $plan->slug])}}" class="btn btn-primary">Vedi dettagli piano</a></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
 
     </div>
 
