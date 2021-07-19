f81219274ccb3347484a9be92bb77219348a6d33
calavera <fmontani81@gmail.com>
2021-07-19 09:43:35 +0200

    aggiunte tutte categorie a categorie in file linguistici

--- resources/lang/it/categories.php
+++ resources/lang/it/categories.php
@@ -1,4 +1,16 @@
 <?php 
 return [
+    'violinista' => 'violinista|violinisti',
     'chitarrista' => 'chitarrista|chitarristi',
+    'dj' => 'dj|dj',
+    'cantante' => 'cantante|cantanti',
+    'batterista' => 'batterista|batteristi',
+    'bassista' => 'bassista|bassisti',
+    'chitarrista' => 'chitarrista|chitarristi',
+    'sassofonista' => 'sassofonista|sassofonisti',
+    'rec & mix' => 'rec & mix|rec & mix',
+    'percussionista' => 'percussionista|percussionista',
+    'tastierista' => 'tastierista|tastieristi',
+    'trombettista' => 'trombettista|trombettisti',
+    'flautista' => 'flautista|flautisti'
 ];
