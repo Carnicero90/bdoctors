89c11134374c0ab04c50ff50383811f30154d558
Mattia Bocchino <pmattiab@live.it>
2021-07-22 17:08:22 +0200

    aggiustate card categories

--- resources/views/partials/categories-list.blade.php
+++ resources/views/partials/categories-list.blade.php
@@ -1,6 +1,6 @@
 <div class="col-3">
     <a href="{{route("category-page", ["slug" => $category->slug])}}" class="font-weight-bold no-decorations">
-        <div style="background-image: url({{ asset('/img/categories_img/' . config('categories.' . $category->slug . '.img')) }})" class="card mb-3 zoom cat-card shadow">
+        <div style="background-image: url({{ asset('/img/categories_img/' . config('categories.' . $category->slug . '.img')) }})" class="card mb-3 zoom cat-card">
             <div class="text-center text-uppercase cat-icon-container">
                 <span class="cat-list-title">{{str_replace("registrazione e mixaggio", "rec & mix", $category->name)}}</span>
             </div>
