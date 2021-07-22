4f81eeffed861bca345e540e07ab0df7d4ac6020
Andrea Esposito <andexpox@gmail.com>
2021-07-22 19:06:15 +0200

    card-category mod

--- resources/views/partials/categories-list.blade.php
+++ resources/views/partials/categories-list.blade.php
@@ -1,6 +1,6 @@
 <div class="col-3">
     <a href="{{route("category-page", ["slug" => $category->slug])}}" class="font-weight-bold no-decorations">
-        <div style="background-image: url({{ asset('/img/categories_img/' . config('categories.' . $category->slug . '.img')) }})" class="card mb-3 zoom cat-card">
+        <div style="background-image: url({{ asset('/img/categories_img/' . config('categories.' . $category->slug . '.img')) }})" class="card mb-4 zoom cat-card">
             <div class="text-center text-uppercase cat-icon-container">
                 <span class="cat-list-title cat-header">{{str_replace("registrazione e mixaggio", "rec & mix", $category->name)}}</span>
             </div>
