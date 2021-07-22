2c7d5177acff6c81de17df0ceff2ff4db212c468
Andrea Esposito <andexpox@gmail.com>
2021-07-22 12:29:45 +0200

    add favicon

--- public/js/home.js
+++ public/js/home.js
@@ -277,7 +277,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/filippomontani/becci/bdoctors/resources/js/guest/home.js */"./resources/js/guest/home.js");
+module.exports = __webpack_require__(/*! C:\laravel-projects\bdoctors-1\resources\js\guest\home.js */"./resources/js/guest/home.js");
 
 
 /***/ })
