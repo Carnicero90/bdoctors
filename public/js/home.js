fc97fd42fca4b75e5317e0d8a2073fc8ae59171b
TommasoAgo <74262446+TommasoAgo@users.noreply.github.com>
2021-07-23 09:42:41 +0200

    cambiato font h2 ecc

--- public/js/home.js
+++ public/js/home.js
@@ -275,7 +275,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/filippomontani/becci/bdoctors/resources/js/guest/home.js */"./resources/js/guest/home.js");
+module.exports = __webpack_require__(/*! /Users/Tom/BDoctors/bdoctors/resources/js/guest/home.js */"./resources/js/guest/home.js");
 
 
 /***/ })
