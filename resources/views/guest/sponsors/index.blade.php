04bf80042e7df84edde78e29007608de197219a7
calavera <fmontani81@gmail.com>
2021-07-29 10:41:28 +0200

    reinserite immagini e eliminata pagina index per piani sponsorizzazione

--- resources/views/guest/sponsors/index.blade.php
+++ resources/views/guest/sponsors/index.blade.php
@@ -2,32 +2,43 @@
 
 @section('content')
 
-<div class="top-margine">
-    <div class="container">
+    <div class="top-margine">
+        <div class="container">
 
-        <div class="mt-3 mb-4">
-            <h1>Piani di sponsorizzazione</h1>
-        </div>
-        
-        <div class="row">
-            @foreach ($all_plans as $plan)
-                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                    <div class="card mb-4">
-                        <div class="card-header d-flex align-items-center justify-content-between">
-                            <h3 class="card-title">{{ucfirst(str_replace("_", " ", $plan->name))}}</h3>
-                            <h5>€ {{$plan->pricing}}</h5>
-                        </div>
-                        <div class="card-body">
-                            <div class="mb-2"><p class="card-text">{{$plan->description}}</p></div>
-                            <div class="mb-4"><span class="card-text">Durata {{$plan->duration_in_hours}} ore</span></div>
-                            <div><a href="{{route('sponsor-show', ['slug' => $plan->slug])}}" class="btn btn-outline-primary">Vedi dettagli piano</a></div>
+            <div class="mt-3 mb-4">
+                <h1>Piani di sponsorizzazione</h1>
+            </div>
+
+            <div class="row">
+                @foreach ($all_plans as $plan)
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="card mb-4">
+                            <div class="card-header d-flex align-items-center justify-content-between">
+                                <h3 class="card-title">{{ ucfirst(str_replace('_', ' ', $plan->name)) }}</h3>
+                                <h5>€ {{ $plan->pricing }}</h5>
+                            </div>
+                            <div class="card-body">
+                                <div class="mb-2">
+                                    <p class="card-text">{{ $plan->description }}</p>
+                                </div>
+                                <div class="mb-4"><span class="card-text">Durata {{ $plan->duration_in_hours }} ore</span>
+                                </div>
+
+                                @if (Auth::user())
+                                    <div><a href="{{ route('admin.pay', ['id' => $plan->id]) }}"
+                                            class="btn btn-outline-success">Acquista a soli €
+                                            {{ $plan->pricing }}</a></div>
+                                @else
+                                    <div><a href="{{ route('register') }}" class="btn btn-outline-success">Iscriviti per
+                                            accedere ai nostri piani premium</a></div>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
 
+        </div>
     </div>
-</div>
 
 @endsection
